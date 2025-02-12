1133caf226a51928f11c0e33d3e3d390dda57d4d
maheesh <maheesh@gmail.com>
2025-02-13 02:38:32 +0530

    test command in attendance,php

--- plugins/mod/attendance/attendance.php
+++ plugins/mod/attendance/attendance.php
@@ -13,7 +13,7 @@
 //s
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
+//this is test command
 /**
  * Prints attendance info for particular user
  *
