f70a9fd19feb81c5b79b0da7136c35bc3ca78e0b
maheesh <maheesh@gmail.com>
2025-02-13 15:24:06 +0530

    c

--- plugins/auth/test_settings.php
+++ plugins/auth/test_settings.php
@@ -13,7 +13,7 @@
 //s
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-//Mahe
+//Mahestest
 // /**
  * Test auth settings.
  *
