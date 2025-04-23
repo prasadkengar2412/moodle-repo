d59c87efe059c30a9ac0aa7a5cd5f8997680e591
maheesh <maheesh@gmail.com>
2025-04-23 16:08:29 +0530

    1. Updated attendance version.php 2. Fixed ..... 3. Added .....

--- plugins/mod/attendance/version.php
+++ plugins/mod/attendance/version.php
@@ -23,7 +23,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2025042301;
+$plugin->version  = 2025042302;
 $plugin->requires = 2022031100; // Requires 4.0.
 $plugin->release = '4.0.11';
 $plugin->maturity  = MATURITY_STABLE;
