f8517aa532c4aa51b39ca8f6a052fc5a49b8066f
maheesh <maheesh@gmail.com>
2025-04-25 14:37:48 +0530

    updated verison

--- plugins/enrol/category/version.php
+++ plugins/enrol/category/version.php
@@ -13,7 +13,7 @@
 //test comment
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-//TEST 
+//TEST MAHESH
 /**
  * Category enrolment plugin version specification.
  *
