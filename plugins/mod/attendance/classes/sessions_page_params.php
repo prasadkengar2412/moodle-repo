ee93e06f3abc4fd31ddb144efc04bcec3833d212
prasadkengar2412 <109298995+prasadkengar2412@users.noreply.github.com>
2025-06-16 08:53:42 +0530

    Update sessions_page_params.php
    newline added in pluginsmod-attendance-classes-sessionpageparams

--- plugins/mod/attendance/classes/sessions_page_params.php
+++ plugins/mod/attendance/classes/sessions_page_params.php
@@ -13,7 +13,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
+// new line added
 
 /**
  * Class definition for mod_attendance_sessions_page_params
